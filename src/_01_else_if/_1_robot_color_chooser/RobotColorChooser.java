429c03f2ebe3a90ea48c0eee46d8ce4143e6fbd5
league <league@192.168.1.135>
2026-03-20 17:01:51 -0700

    1234

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -2,14 +2,30 @@
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import java.awt.AWTException;
 import java.awt.Color;
+
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		
 		//1. Create a new Robot
+	
+			Robot bob = new Robot();
+			bob.penDown();
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
+			bob.turn(90);
+			bob.move(100);
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
 		//3. Set the pen width to 10
+			bob.setPenWidth(10);
 		//4. Ask the user what color pen they would like the robot to draw with
 		//5. Use an if/else statement to set the pen color that the user requested
         //6. If the user doesn't enter anything, choose a random color
